3b20a796143cf03012f3cfc01582c2997deca7af
Althafsajeed <althafsajeed131@gmail.com>
2026-06-07 23:57:44 +0530

    Run migrations during Render build

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py migrate --noinput
+python manage.py create_default_admin --username "${ADMIN_USERNAME:-admin}" --password "${ADMIN_PASSWORD:-admin}"
