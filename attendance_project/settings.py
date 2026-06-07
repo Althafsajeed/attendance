30b30e9546588cccf02bcd5fee681791d379dd2c
Althafsajeed <althafsajeed131@gmail.com>
2026-06-07 23:15:02 +0530

    Use Render database compatibility settings

--- attendance_project/settings.py
+++ attendance_project/settings.py
@@ -11,7 +11,7 @@ DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = [
     host.strip()
-    for host in os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+    for host in os.environ.get("ALLOWED_HOSTS", "*").split(",")
     if host.strip()
 ]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
